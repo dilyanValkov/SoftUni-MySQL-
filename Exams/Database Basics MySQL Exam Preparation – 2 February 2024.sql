c1c78fbdb52ae67781296136365338556d83011c
dilyanValkov <dilqnvalkov@gmail.com>
2024-02-04 22:03:29 +0200

    Create Database Basics MySQL Exam Preparation – 2 February 2024.sql

---
+++ Exams/Database Basics MySQL Exam Preparation – 2 February 2024.sql
@@ -0,0 +1,134 @@
+CREATE SCHEMA universities_db;
+
+#01. Table Design
+CREATE TABLE `countries`(
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (40) UNIQUE NOT NULL);
+
+CREATE TABLE `cities`(
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (40) UNIQUE NOT NULL,
+`population` INT,
+`country_id` INT NOT NULL,
+CONSTRAINT fk_cities_countries
+FOREIGN KEY (`country_id`)
+REFERENCES countries(`id`));
+
+CREATE TABLE `universities`(
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (60) UNIQUE NOT NULL,
+`address` VARCHAR (80) UNIQUE NOT NULL,	
+`tuition_fee` DECIMAL(19,2) NOT NULL,
+`number_of_staff` INT,
+`city_id` INT,
+CONSTRAINT fk_univers_cities
+FOREIGN KEY (`city_id`)
+REFERENCES cities(`id`));
+
+CREATE TABLE `students`(
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`first_name` VARCHAR (40) NOT NULL,
+`last_name` VARCHAR (40) NOT NULL,
+`age` INT,
+`phone` VARCHAR (20) UNIQUE NOT NULL,	
+`email`VARCHAR (255) UNIQUE NOT NULL,	
+`is_graduated` BOOLEAN NOT NULL,
+`city_id` INT,
+CONSTRAINT fk_students_cities
+FOREIGN KEY (`city_id`)
+REFERENCES cities(`id`));
+
+CREATE TABLE `courses`(
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR (40) UNIQUE NOT NULL,
+`duration_hours` DECIMAL(19,2),
+`start_date` DATE,	
+`teacher_name`VARCHAR (60) UNIQUE NOT NULL,	
+`description` TEXT,
+`university_id` INT,
+CONSTRAINT fk_courses_universiteties
+FOREIGN KEY (`university_id`)
+REFERENCES universities(`id`));
+
+CREATE TABLE `students_courses`(
+`grade` DECIMAL(19,2) NOT NULL,
+`student_id` INT NOT NULL,
+`course_id` INT NOT NULL,
+CONSTRAINT fk_sc_students
+FOREIGN KEY (`student_id`)
+REFERENCES students(`id`),
+CONSTRAINT fk_sc_courses
+FOREIGN KEY (`course_id`)
+REFERENCES courses(`id`));
+
+#02. Insert
+INSERT INTO `courses` (`name`,`duration_hours`,`start_date`,`teacher_name`,`description`,`university_id`)(
+SELECT CONCAT(`teacher_name`, ' ', 'course'),
+		CHAR_LENGTH(`name`)/10,
+        DATE_ADD(`start_date`, INTERVAL 5 DAY),
+        REVERSE(`teacher_name`),
+        CONCAT('Course ', `teacher_name`, REVERSE(`description`)),
+        DAY(`start_date`)
+        FROM `courses`
+        WHERE `id` <= 5);
+        
+ #03. Update
+UPDATE `universities` 
+SET 
+    `tuition_fee` = `tuition_fee` + 300
+WHERE
+    `id` BETWEEN 5 AND 12;
+    
+#04. Delete
+DELETE u FROM `universities` AS u 
+WHERE
+    `number_of_staff` IS NULL;
+    
+#05. Cities
+SELECT 
+    `id`, `name`, `population`, `country_id`
+FROM
+    `cities`
+ORDER BY `population` DESC;
+
+#06. Students age
+SELECT 
+    `first_name`, `last_name`, `age`, `phone`, `email`
+FROM
+    `students`
+WHERE
+    `age` >= 21
+ORDER BY `first_name` DESC , `email` , `id`
+LIMIT 10;
+
+#07. New students
+SELECT CONCAT_WS(' ' , c.`first_name`, c.`last_name`) AS `full_name`,
+substring(c.`email`,2,10),
+REVERSE(c.`phone`) AS 'password'
+FROM `students` AS c
+LEFT JOIN `students_courses` AS sc ON c.`id` = sc.`student_id`
+WHERE sc.`course_id` IS NULL
+ORDER BY `password` DESC;
+
+#08. Students count
+SELECT 
+    COUNT(s.`id`) AS 'students_count',
+    u.`name` AS 'universotety_name'
+FROM
+    `students` AS s
+         JOIN
+    `students_courses` AS sc ON s.`id` = sc.`student_id`
+         JOIN
+    `courses` AS c ON c.`id` = sc.`course_id`
+        RIGHT JOIN
+    `universities` AS u ON u.`id` = c.`university_id`
+GROUP BY u.`name`
+HAVING students_count >= 8
+ORDER BY students_count DESC , universotety_name DESC;
+
+#09. Price rankings
+
+
+
+    
+    
